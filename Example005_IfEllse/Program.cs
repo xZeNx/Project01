399d1ff754e82f8d283b7d0ff1384c76861dc309
ZeN <zensmi@yandex.ru>
2022-08-04 16:11:25 +0300

    add Example005_IfEllse

---
+++ Example005_IfEllse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Представтесь:");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "сергей")
+{
+    Console.WriteLine("Приветствую Серёга!");
+}
+else
+{
+    Console.Write("Привет,");
+    Console.WriteLine(username);
+}
