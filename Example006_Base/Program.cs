bf75e762b8ee304dadc6ca8ffd37bbeac3b0be53
ZeN <zensmi@yandex.ru>
2022-08-04 16:26:22 +0300

    add Example006_Base

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 1;
+int b = 3;
+int c = 7;
+int d = 10;
+int e = 5;
+
+int max = a;
+
+if (a>max ) max=a;
+if (b>max ) max=b;
+if (c>max ) max=c;
+if (d>max ) max=d;
+if (e>max ) max=e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
